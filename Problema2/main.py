9ac38a0d95f30433976ca933cd064d9c43a0a7e9
alexlomu <91721507+alexlomu@users.noreply.github.com>
2023-04-24 18:25:17 +0200

    guardamos carpeta

---
+++ Problema2/main.py
@@ -0,0 +1,11 @@
+from Problema2.clases import *
+if __name__ == "__main__":
+    
+    player1 = Player("Flecha", 10000)
+    player2 = Player("Alex", 10000)
+    player3 = Player("Carlos", 10000)
+    player4 = Player("German", 10000)
+
+    Game([player1, player2, player3, player4], 50000)
+    Game.play_even_odd(player1)
+    
